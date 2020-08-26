a090c64dab4052079d56266649b3ad40318bb67c
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-08-25 21:44:33 -0500

    second

---
+++ hello_name.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name? ")
+
+birth_month = input("What month were born in? ")
+
+print("Hello, " + name)
+print("Happy birthday " + birth_month)
+print("There are " +str(len(name)) + "letters in your name ")
